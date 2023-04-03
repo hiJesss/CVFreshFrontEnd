2cb2c7e35c7867bf61cb04126ee079feeab99d6e
hiJess <hiitsjesssss@gmail.com>
2023-04-03 08:40:40 +0000

    Initial project, firebase semi-integrated

---
+++ app/src/main/java/com/st10083342testapp1/cvfreshfrontend/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.st10083342testapp1.cvfreshfrontend
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
